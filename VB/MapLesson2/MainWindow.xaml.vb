f8989fce8d6f31f248c23086202afc8aa41499c7
DevExpressExampleBot <fakeEmail>
2022-12-18 21:35:36 +0400

    Source auto update [skip ci]

--- VB/MapLesson2/MainWindow.xaml.vb
+++ VB/MapLesson2/MainWindow.xaml.vb
@@ -1,25 +1,13 @@
-﻿Imports DevExpress.Xpf.Map
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace MapLesson2
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
